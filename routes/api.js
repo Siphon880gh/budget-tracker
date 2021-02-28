b34c9b21bbb176fa4ebf6058c49f3837699e5900
Weng Fei Fung <weffung@ucdavis.edu>
2021-02-27 22:54:59 -0800

    Basic browser test setup (http://localhost:3001/test/)

--- routes/api.js
+++ routes/api.js
@@ -1,34 +1,41 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
+const path = require("path");
 
-router.post("/api/transaction", ({body}, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+// Test
+router.get("/test", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../__test", "index.html"));
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+// Restful API
+router.post("/api/transaction", ({ body }, res) => {
+    Transaction.create(body)
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
+});
+
+router.post("/api/transaction/bulk", ({ body }, res) => {
+    Transaction.insertMany(body)
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
 });
 
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+    Transaction.find({}).sort({ date: -1 })
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
@@ -15,6 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(express.static("__test"));
+// app.use(express.static("__test", "*"));
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
